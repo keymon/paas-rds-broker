94db4be11864adf0028af6340aa831e0724dd584
Joshua Carp <jm.carp@gmail.com>
2016-12-24 19:07:33 -0500

    Manage postgres users with event triggers.

--- sqlengine/postgres_engine.go
+++ sqlengine/postgres_engine.go
@@ -9,35 +9,24 @@ import (
 	"github.com/pivotal-golang/lager"
 )
 
-const defaultStateDBName = "broker_state"
-
 type PostgresEngine struct {
-	logger             lager.Logger
-	stateEncryptionKey string
-	db                 *sql.DB
-	address            string
-	port               int64
-	username           string
-	password           string
-	requireSSL         bool
-	stateDBName        string
+	logger     lager.Logger
+	db         *sql.DB
+	groupName  string
+	requireSSL bool
 }
 
-func NewPostgresEngine(logger lager.Logger, stateEncryptionKey string) *PostgresEngine {
+func NewPostgresEngine(logger lager.Logger, groupName string) *PostgresEngine {
 	return &PostgresEngine{
-		logger:             logger.Session("postgres-engine"),
-		stateEncryptionKey: stateEncryptionKey,
-		requireSSL:         true,
-		stateDBName:        defaultStateDBName,
+		logger:     logger.Session("postgres-engine"),
+		groupName:  groupName,
+		requireSSL: true,
 	}
 }
 
 func (d *PostgresEngine) Open(address string, port int64, dbname string, username string, password string) error {
-	d.address, d.port, d.username, d.password = address, port, username, password
-	var (
-		connectionString          = d.URI(address, port, dbname, username, password)
-		sanitizedConnectionString = d.URI(address, port, dbname, username, "REDACTED")
-	)
+	connectionString := d.URI(address, port, dbname, username, password)
+	sanitizedConnectionString := d.URI(address, port, dbname, username, "REDACTED")
 	d.logger.Debug("sql-open", lager.Data{"connection-string": sanitizedConnectionString})
 
 	db, err := sql.Open("postgres", connectionString)
@@ -70,63 +59,30 @@ func (d *PostgresEngine) Close() {
 }
 
 func (d *PostgresEngine) CreateUser(bindingID, dbname string) (username, password string, err error) {
-	stateDB, err := d.openStateDB(d.logger, d.stateEncryptionKey)
-	if err != nil {
+	if err := d.ensureGroup(dbname); err != nil {
 		return "", "", err
 	}
-	defer stateDB.Close()
 
-	tx, err := stateDB.Begin()
-	if err != nil {
-		stateDB.logger.Error("sql-error", err)
+	if err := d.ensureTrigger(); err != nil {
 		return "", "", err
 	}
-	commitCalled := false
-	defer func() {
-		if !commitCalled {
-			tx.Rollback()
-		}
-	}()
 
-	username = generatePostgresUsername(dbname)
-
-	password, ok, err := stateDB.fetchUserPassword(username)
-	if err != nil {
-		return "", "", err
-	}
-	if ok {
-		// User already exists. Nothing further to do.
-		return username, password, nil
-	}
-
-	// No user exists, proceed with creation.
-
+	username = generateUsername(bindingID)
 	password = generatePassword()
-	var (
-		createUserStatement          = "CREATE USER \"" + username + "\" WITH PASSWORD '" + password + "'"
-		sanitizedCreateUserStatement = "CREATE USER \"" + username + "\" WITH PASSWORD 'REDACTED'"
-	)
-	d.logger.Debug("create-user", lager.Data{"statement": sanitizedCreateUserStatement})
-	if _, err := tx.Exec(createUserStatement); err != nil {
+
+	createUserStatement := fmt.Sprintf(`create role "%s" inherit login password '%s'`, username, password)
+	d.logger.Debug("create-user", lager.Data{"statement": createUserStatement})
+
+	if _, err := d.db.Exec(createUserStatement); err != nil {
 		d.logger.Error("sql-error", err)
 		return "", "", err
 	}
 
-	grantPrivilegesStatement := "GRANT ALL PRIVILEGES ON DATABASE \"" + dbname + "\" TO \"" + username + "\""
+	grantPrivilegesStatement := fmt.Sprintf(`grant "%s" to "%s"`, d.groupName, username)
 	d.logger.Debug("grant-privileges", lager.Data{"statement": grantPrivilegesStatement})
-	if _, err := tx.Exec(grantPrivilegesStatement); err != nil {
-		d.logger.Error("sql-error", err)
-		return "", "", err
-	}
 
-	err = stateDB.storeUser(username, password)
-	if err != nil {
-		return "", "", err
-	}
-	err = tx.Commit()
-	commitCalled = true // Prevent Rollback being called in deferred function
-	if err != nil {
-		d.logger.Error("commit.sql-error", err)
+	if _, err := d.db.Exec(grantPrivilegesStatement); err != nil {
+		d.logger.Error("sql-error", err)
 		return "", "", err
 	}
 
@@ -134,7 +90,13 @@ func (d *PostgresEngine) CreateUser(bindingID, dbname string) (username, passwor
 }
 
 func (d *PostgresEngine) DropUser(bindingID string) error {
-	// For PostgreSQL we don't drop the user because we retain a single user for all bound applications
+	username := generateUsername(bindingID)
+	dropUserStatement := fmt.Sprintf(`drop role "%s"`, username)
+
+	if _, err := d.db.Exec(dropUserStatement); err != nil {
+		d.logger.Error("sql-error", err)
+		return err
+	}
 
 	return nil
 }
@@ -204,4 +166,72 @@ func (d *PostgresEngine) JDBCURI(address string, port int64, dbname string, user
 // will be persisted across multiple application bindings
 func generatePostgresUsername(dbname string) string {
 	return dbname + "_owner"
+}
+
+func (d *PostgresEngine) ensureGroup(dbname string) error {
+	ensureGroupStatement := fmt.Sprintf(`
+		do
+		$body$
+		begin
+			if not exists (select * from pg_catalog.pg_roles where rolname = '%s') then
+				create role "%s";
+			end if;
+			grant all privileges on database "%s" to "%s";
+		end
+		$body$
+	`, d.groupName, d.groupName, dbname, d.groupName)
+	d.logger.Debug("ensure-group", lager.Data{"statement": ensureGroupStatement})
+
+	if _, err := d.db.Exec(ensureGroupStatement); err != nil {
+		d.logger.Error("sql-error", err)
+		return err
+	}
+
+	return nil
+}
+
+func (d *PostgresEngine) ensureTrigger() error {
+	tx, err := d.db.Begin()
+	if err != nil {
+		return err
+	}
+
+	commitCalled := false
+	defer func() {
+		if !commitCalled {
+			tx.Rollback()
+		}
+	}()
+
+	cmds := []string{
+		fmt.Sprintf(`
+			create or replace function reassign_owned() returns event_trigger language plpgsql as $$
+			begin
+				if pg_has_role(current_user, '%s', 'member') then
+					execute 'reassign owned by "' || current_user || '" to "%s"';
+				end if;
+			end
+			$$;
+		`, d.groupName, d.groupName),
+		`drop event trigger if exists reassign_owned;`,
+		`create event trigger reassign_owned on ddl_command_end execute procedure reassign_owned();`,
+	}
+
+	for _, cmd := range cmds {
+		d.logger.Debug("ensure-trigger", lager.Data{"statement": cmd})
+		_, err = tx.Exec(cmd)
+		if err != nil {
+			d.logger.Error("sql-error", err)
+			return err
+		}
+	}
+
+	err = tx.Commit()
+	commitCalled = true
+	if err != nil {
+		d.logger.Error("commit.sql-error", err)
+		return err
+	}
+
+	return nil
 }
